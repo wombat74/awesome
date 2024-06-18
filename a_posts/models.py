aa468ada2acf21bbc2dea96295e411b1160eea66
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2024-06-17 17:18:50 -0700

    added tags table

--- a_posts/models.py
+++ a_posts/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     url = models.CharField(max_length=500, null=True)
     image = models.URLField(max_length=500)
     body = models.TextField()
+    tags = models.ManyToManyField('Tag')
     created = models.DateTimeField(auto_now_add=True)
     id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
@@ -14,3 +15,10 @@ class Post(models.Model):
     
     class Meta:
         ordering = ['-created']
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
