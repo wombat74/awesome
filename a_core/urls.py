cc58460d608a12f01f38d26e5a609aabd17c2077
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2024-06-25 16:50:59 -0700

    added file upload capability. Added static and media folders

--- a_core/urls.py
+++ a_core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from a_posts.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,6 @@ urlpatterns = [
     path('post/edit/<pk>/', post_edit_view, name='post-edit'),
     path('post/<pk>/', post_page_view, name='post'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- a_posts/models.py
+++ a_posts/models.py
@@ -18,7 +18,12 @@ class Post(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=20)
+    image  = models.FileField(upload_to='icons/', null=True, blank=True)
     slug = models.SlugField(max_length=20, unique=True)
+    order = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        ordering = ['order']
