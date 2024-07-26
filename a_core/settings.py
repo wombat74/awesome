d05819e5bf3e44583fb9ff55900d58f3d6c1d562
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2024-07-26 16:05:44 -0700

    added account pages

--- a_core/settings.py
+++ a_core/settings.py
@@ -153,3 +153,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+
+# eddie, User1_User1
