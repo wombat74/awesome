71ef85f5ec5c7035aa029acbf3fe2157fdbb54b1
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2024-08-26 17:03:43 -0700

    added likes decorator functionality

--- a_core/urls.py
+++ a_core/urls.py
@@ -14,16 +14,17 @@ urlpatterns = [
     path('post/delete/<pk>/', post_delete_view, name='post-delete'),
     path('post/edit/<pk>/', post_edit_view, name='post-edit'),
     path('post/<pk>/', post_page_view, name='post'),
-    path('post/<pk>/like/', like_post, name='like-post'),
+    path('post/like/<pk>/', like_post, name='like-post'),
+    path('comment/like/<pk>/', like_comment, name='like-comment'),
     path('profile/', profile_view, name='profile'),
     path('<username>/', profile_view, name='userprofile'),
     path('profile/edit/', profile_edit_view, name='profile-edit'),
     path('profile/delete/', profile_delete_view, name='profile-delete'),
     path('profile/onboarding/', profile_edit_view, name="profile-onboarding"),
-    path('commentsent/<pk>', comment_sent, name="comment-sent"),
-    path('comment/delete/<pk>', comment_delete_view, name="comment-delete"),
-    path('reply-sent/<pk>', reply_sent, name="reply-sent"),
-    path('reply/delete/<pk>', reply_delete_view, name="reply-delete"),
+    path('commentsent/<pk>/', comment_sent, name="comment-sent"),
+    path('comment/delete/<pk>/', comment_delete_view, name="comment-delete"),
+    path('reply-sent/<pk>/', reply_sent, name="reply-sent"),
+    path('reply/delete/<pk>/', reply_delete_view, name="reply-delete"),
 ]
 
 if settings.DEBUG:
