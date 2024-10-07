243868b89a1fe4084018f814f552ba7b0f631147
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2024-10-07 16:26:35 -0700

    added chat functionality

--- a_inbox/urls.py
+++ a_inbox/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', inbox_view, name='inbox'),
     path('c/<conversation_id>/', inbox_view, name='inbox'),
+    path('search_users/', search_users, name='inbox-searchusers'),
+    path('new_message/<recipient_id>', new_message, name='inbox-newmessage'),
+    path('new_reply/<conversation_id>', new_reply, name='inbox-newreply'),
 ]
--- a_users/views.py
+++ a_users/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.db.models import Count
 from django.http import Http404
 from django.contrib.auth.models import User
+from a_inbox.forms import InboxNewMessageForm
 from a_posts.forms import *
 from .forms import *
 
@@ -31,9 +32,12 @@ def profile_view(request, username=None):
             posts = profile.user.likedposts.order_by('-likedpost__created')
         return render(request, 'snippets/loop_profile_posts.html', {'posts': posts})
 
+    new_message_form = InboxNewMessageForm()
+    
     context = {
         'profile': profile,
-        'posts': posts
+        'posts': posts,
+        'new_message_form': new_message_form,
     }
     return render(request, 'a_users/profile.html', context)
 
